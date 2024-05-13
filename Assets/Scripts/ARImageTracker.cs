28b646b2a5e1f27f2bf9c6d829d9245e6989a3b3
Sajmon101 <szymk4@wp.pl>
2024-05-13 22:20:41 +0200

    Creation animal second time bug fixed

--- Assets/Scripts/ARImageTracker.cs
+++ Assets/Scripts/ARImageTracker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ARImageTracker : MonoBehaviour
 {
@@ -10,17 +11,21 @@ public class ARImageTracker : MonoBehaviour
     private ARTrackedImageManager trackedImageManager;
 
     public Dictionary<string, GameObject> prefabs = new Dictionary<string, GameObject>();
+    private Dictionary<string, GameObject> trackedInstances = new Dictionary<string, GameObject>();
 
     private List<GameObject> allInstances = new List<GameObject>();
+    private AudioSource confirmSound;
     [SerializeField] private Button removeButton;
     [SerializeField] GameObject prefabElephant;
     [SerializeField] GameObject prefabPanda;
     [SerializeField] GameObject prefabZebra;
+    [SerializeField] Text debugText;
 
     void Awake()
     {
         trackedImageManager.trackedImagesChanged += OnImageChanged;
         removeButton.onClick.AddListener(RemoveAllInstances);
+        confirmSound = GetComponent<AudioSource>();
 
     }
 
@@ -31,27 +36,60 @@ public class ARImageTracker : MonoBehaviour
         prefabs.Add("Zebra", prefabZebra);
     }
 
-
     void OnImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (var trackedImage in eventArgs.added)
         {
-            if (prefabs.TryGetValue(trackedImage.referenceImage.name, out GameObject prefab))
+            if (!trackedInstances.ContainsKey(trackedImage.referenceImage.name))
             {
-                var instance = Instantiate(prefab, trackedImage.transform.position, Quaternion.identity);
-                allInstances.Add(instance);
+                InstantiatePrefab(trackedImage);
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.updated)
+        {
+            if (trackedImage.trackingState == UnityEngine.XR.ARSubsystems.TrackingState.Tracking &&
+                !trackedInstances.ContainsKey(trackedImage.referenceImage.name))
+            {
+                InstantiatePrefab(trackedImage);
+            }
+        }
+
+        foreach (var trackedImage in eventArgs.removed)
+        {
+            if (trackedInstances.TryGetValue(trackedImage.referenceImage.name, out GameObject instance))
+            {
+                Destroy(instance);
+                trackedInstances.Remove(trackedImage.referenceImage.name);
+            }
+        }
+    }
+
+    private void InstantiatePrefab(ARTrackedImage trackedImage)
+    {
+        if (prefabs.TryGetValue(trackedImage.referenceImage.name, out GameObject prefab))
+        {
+            var instance = Instantiate(prefab, trackedImage.transform.position, Quaternion.identity);
+            trackedInstances[trackedImage.referenceImage.name] = instance;
+            debugText.text = prefab.name;
+
+            if (!confirmSound.isPlaying)
+            {
+                confirmSound.Play();
             }
         }
     }
 
     public void RemoveAllInstances()
     {
-        foreach (var instance in allInstances)
+        foreach (var instance in trackedInstances.Values)
         {
             if (instance != null)
+            {
                 Destroy(instance);
+            }
         }
-        allInstances.Clear(); // Wyczyœæ listê po usuniêciu wszystkich instancji
+        trackedInstances.Clear();
     }
 
     void OnDestroy()
