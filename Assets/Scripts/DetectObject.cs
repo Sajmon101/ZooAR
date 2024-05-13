fbcf5021d77e0982671ea9647daa4363095a751c
Sajmon101 <szymk4@wp.pl>
2024-05-13 22:47:43 +0200

    Message after clicking in surface

--- Assets/Scripts/DetectObject.cs
+++ Assets/Scripts/DetectObject.cs
@@ -1,8 +1,42 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using UnityEngine.XR.ARFoundation;
+//public class DetectObject : MonoBehaviour
+//{
+//    public Text debugText;
+//    Camera arCam;
+
+//    void Start()
+//    {
+//        arCam = GameObject.Find("AR Camera").GetComponent<Camera>();
+//    }
+
+//    void Update()
+//    {
+//        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+//        {
+//            Ray ray = arCam.ScreenPointToRay(Input.GetTouch(0).position);
+//            RaycastHit hit;
+//            if (Physics.Raycast(ray, out hit))
+//            {
+//                AudioSource audio = hit.transform.GetComponent<AudioSource>();
+//                if (audio != null && !audio.isPlaying)
+//                {
+//                    audio.Play();
+//                }
+//            }
+//        }
+//    }
+//}
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
+
 public class DetectObject : MonoBehaviour
 {
     public Text debugText;
@@ -21,10 +55,18 @@ public class DetectObject : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                AudioSource audio = hit.transform.GetComponent<AudioSource>();
-                if (audio != null && !audio.isPlaying)
+                // SprawdŸ, czy trafiony obiekt to powierzchnia AR
+                if (hit.transform.GetComponent<ARPlane>() != null)
                 {
-                    audio.Play();
+                    debugText.text = "Trafiono w powierzchniê AR";
+                }
+                else
+                {
+                    AudioSource audio = hit.transform.GetComponent<AudioSource>();
+                    if (audio != null && !audio.isPlaying)
+                    {
+                        audio.Play();
+                    }
                 }
             }
         }
